24f1618d418473b11e9aba0e3c950c61809ee595
philipiaeveline <philipiaeveline13@gmail.com>
2021-01-11 14:36:48 +0300

    add

--- pictures/views.py
+++ pictures/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render
 from .models import Image, Location
 
 
-# def index(request):
-#     images = Image.objects.all()
-#     locations = Location.get_locations()
-#     print(locations)
-#     return render(request, 'index.html', {'images': images[::-1], 'locations': locations})
+def index(request):
+    images = Image.objects.all()
+    locations = Location.get_locations()
+    print(locations)
+    return render(request, 'index.html', {'images': images[::-1], 'locations': locations})
 
 
 def image_location(request, location):
